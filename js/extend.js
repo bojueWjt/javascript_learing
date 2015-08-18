caaa7f37c980dcfc00817242d81f0d55b2dbaeed
wjt <627937064@qq.com>
2015-08-18 16:25:39 +0800

    原型、组合、混合继承模式

---
+++ js/extend.js
@@ -0,0 +1,69 @@
+//原型继承
+/*
+function Person(name , age){
+	this.name = name ;
+	this.age = age ;
+}
+
+Person.prototype.id = 10;
+
+function Boy(friend){
+	this.friend = friend;
+}
+
+Boy.prototype = new Person('haiming',21);
+
+var p1 = new Boy('yangtian');
+
+console.log(p1.friend);
+console.log(p1.id);
+console.log(p1.name);
+*/
+
+
+//借用构造函数继承模式
+/*
+function Person(name , age){
+	this.name = name ;
+	this.age = age ;
+}
+
+Person.prototype.id = 10;
+
+function Boy(name , age ,friend){
+	this.friend = friend;
+	Person.call(this,name,age);
+}
+
+var p1 = new Boy('shijie',23,'xixisu');
+
+console.log(p1.friend);
+console.log(p1.name);
+console.log(p1.id);  //显而易见这是undefined
+
+*/
+
+
+//混合继承模式  原型继承 + 借用构造函数继承
+
+
+function Person(name , age){
+	this.name = name;
+	this.age = age;
+}
+
+Person.prototype.id = 10;
+
+function Boy(name , age ,friend){
+	this.name = name ;
+	this.age = age ;
+	this.friend = friend;
+}
+
+Boy.prototype = new Person();
+
+var p1 = new Boy('haiming',23,'shijie');
+
+console.log(p1.name);
+console.log(p1.friend);
+console.log(p1.id);
